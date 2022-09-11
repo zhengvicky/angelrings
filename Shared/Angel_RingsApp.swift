017f4e4bf9e3cc2cc076f7f211d5faee0c40a99e
Vicky Zheng <vickyz@mit.edu>
2022-09-11 17:01:04 -0400

    Initial Commit

---
+++ Shared/Angel_RingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Angel_RingsApp.swift
+//  Shared
+//
+//  Created by Vicky Zheng on 9/11/22.
+//
+
+import SwiftUI
+
+@main
+struct Angel_RingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
